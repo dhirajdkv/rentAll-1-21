4ac5a7cf133d481a1364578f4d12906e6ecc2891
pnaik18081994 <paragnaik2021.git@gmail.com>
2024-05-09 21:25:53 -0700

    Sprint 2
    1. Introduced an intuitive admin panel, enhancing system functionality.
    2. Provided administrators with capabilities to manage product inventory effortlessly.
    3. Implemented robust features enabling addition and deletion of rental products.
    4. Augmented user experience by offering advanced product display options based on both ID and search queries.
    5. Integrated streamlined cart and order functionalities to optimize the overall user journey.

---
+++ server-side/RentAll-branch-5-Final/src/main/java/com/RentAllv1/service/ReviewService.java
@@ -0,0 +1,16 @@
+package com.RentAllv1.service;
+
+import com.RentAllv1.model.Review;
+import com.RentAllv1.model.User;
+import com.RentAllv1.exception.ProductException;
+
+import java.util.List;
+
+public interface ReviewService {
+
+    public Review createReview(ReviewRequest req, User user) throws ProductException;
+
+    public List<Review> getAllReview(Long productId);
+
+
+}
